c0b42eec8375b23b567e03da1736489025df6f49
francoisexpert <fra.expert@gmail.com>
2017-02-13 17:53:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+require('./api/data/db.js');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+var routes = require('./api/routes');
+var routesPublic = require('./api/routes/routesPublic.js');
+
+// Define the port to run on
+app.set('port', 3000);
+
+// Add middleware to console log every request
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
+// Ici on met le système de session
+app.use(session({
+    secret: 'supersecret',
+    resave: true,
+    saveUninitialized: true,
+    httpOnly: false,
+    secure: false
+}));
+
+
+// Set static directory before defining routes
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+
+// Enable parsing of posted forms
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Add some routing
+app.use('/api', routes);
+app.use('/', routesPublic);
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Copy/paste: http://localhost:' + port);
+});
